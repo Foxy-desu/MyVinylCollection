c311e4f02bedc0d74d7bc279b3d294b8bf7e1eea
Alexandra Orliuk <iammeduza@gmail.com>
2024-08-13 22:32:55 +0300

    init: create files

---
+++ src/app/App.tsx
@@ -0,0 +1,13 @@
+import AlbumContainer from '../pages/albums/model/albumsContainer.tsx'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <AlbumContainer/>
+    </>
+  )
+}
+
+export default App
