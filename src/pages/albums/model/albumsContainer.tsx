28c01cabadb2ea30a03f717532a1dbca9f044bd1
Alexandra Orliuk <iammeduza@gmail.com>
2024-08-15 16:04:12 +0300

    feat: add sorting to albums container

--- src/pages/albums/model/albumsContainer.tsx
+++ src/pages/albums/model/albumsContainer.tsx
@@ -6,9 +6,14 @@ import { IAlbum } from "../../../app/store/albumsSlice";
 const AlbumContainer =()=> {
   const albums = useAppSelector(store => store.albums.albumsCollection);
   const [filterQuery, setFilterQuery] = useState('');
+  const [sortMethod, setSortMethod] = useState('artist');
+  const sortedAlbums = useMemo(()=> {
+    const copy = structuredClone(albums);
+    return sortAlbums(copy, sortMethod);
+  }, [albums, sortMethod]);
   const filteredAlbums = useMemo(()=> {
-    return filterAlbumsWithQuery(filterQuery, albums);
-  }, [albums, filterQuery])
+    return filterAlbumsWithQuery(filterQuery, sortedAlbums);
+  }, [sortedAlbums, filterQuery]);
 
   function filterAlbumsWithQuery(query: string, albums: IAlbum[]){
     if(query) {
@@ -27,14 +32,38 @@ const AlbumContainer =()=> {
       return albums;
     }
   };
-
+  function sortAlbums(albums: IAlbum[], sortMethod: string) {
+    switch (sortMethod) {
+      case 'album': {
+        return albums.sort((a, b) => a.album.localeCompare(b.album));
+      }
+      case 'artist': {
+        return albums.sort((a, b) => a.artist.localeCompare(b.artist));
+      }
+      case 'releaseDate': {
+        return albums.sort((a, b) => new Date(a.date).getTime() - new Date(b.date).getTime());
+      }
+      case 'creationDate': {
+        return albums.sort((a, b) => new Date(a.id).getTime() - new Date(b.id).getTime());
+      }
+      case 'none': {
+        return albums;
+      }
+      default: {
+        return albums
+      }
+    }
+  };
   function setFilter(filterQuery:string){
     setFilterQuery(filterQuery.trim().toLowerCase())
-  }
+  };
+  function setSort(sortMethod: string) {
+    setSortMethod(sortMethod);
+  };
 
   return (
     <>
-      <Albums albums={filteredAlbums} filterAlbumsWithQuery={setFilter}/>
+      <Albums albums={filteredAlbums} filterAlbums={setFilter} sortAlbums={setSort}/>
     </>
   )
 };
