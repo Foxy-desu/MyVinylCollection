1aa64658658f08de5be3c0b83ba5b9cd2271a6d9
Alexandra Orliuk <iammeduza@gmail.com>
2024-08-16 21:31:24 +0300

    feat: add middleware and state  preloader

--- src/app/store/store.ts
+++ src/app/store/store.ts
@@ -1,12 +1,31 @@
-import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { Middleware, combineReducers, configureStore } from "@reduxjs/toolkit";
 import albumsReducer from './albumsSlice.ts';
 
 const RootReducer = combineReducers({
   albums: albumsReducer,
 })
 
+const getAlbumsFromLocalStorage = () => {
+  if (
+    localStorage.getItem("albums") !== null &&
+    localStorage.getItem("albums") !== "undefined"
+  ) return JSON.parse(localStorage.getItem("albums")||'{}');
+};
+
+const albumsMiddleware: Middleware = ({ getState }) => {
+  return (next) => (action) => {
+    const result = next(action);
+    localStorage.setItem("albums", JSON.stringify(getState()));
+    return result;
+  };
+};
+
 export const store = configureStore({
   reducer: RootReducer,
+  preloadedState: getAlbumsFromLocalStorage(),
+  middleware: (getDefaultMiddleware)=> {
+    return getDefaultMiddleware().concat(albumsMiddleware)
+  }
 });
 
 export type RootState = ReturnType<typeof store.getState>;
