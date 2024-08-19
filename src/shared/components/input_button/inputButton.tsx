5cdb73af13984057c964996175ce1e678765697b
Alexandra Orliuk <iammeduza@gmail.com>
2024-08-19 23:08:34 +0300

    refactor: move crossElement into input button (to easily manage styling for pseudo classes)

--- src/shared/components/input_button/inputButton.tsx
+++ src/shared/components/input_button/inputButton.tsx
@@ -1,15 +1,25 @@
-import { ReactNode } from 'react';
 import cl from './inputButton.module.scss';
 
 interface IInputButton extends React.ButtonHTMLAttributes<HTMLButtonElement>{
   srText: string;
 }
-const InputButton =({children, srText, ...rest}:IInputButton)=> {
+const InputButton =({srText, ...rest}:IInputButton)=> {
   return(
     <button className={cl.inputButton} {...rest}>
       <span className='visually-hidden'>{srText}</span>
-      {children}
+      <CrossElement rotateDegree={0}/>
     </button>
+  )
+};
+
+const CrossElement=({rotateDegree=0})=>{
+  return (
+    <div className={cl.crossElement}>
+      <div className={cl.linesWrap} style={{transform:`rotate(${rotateDegree}deg)`}}>
+        <span className={cl.crossElement_line}></span>
+        <span className={cl.crossElement_line}></span>
+      </div>
+    </div>
   )
 };
 
