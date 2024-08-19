dd3a74ebb1a533402805a900825facedd09732ff
Alexandra Orliuk <iammeduza@gmail.com>
2024-08-19 23:06:29 +0300

    feat: change form input component realization

--- src/shared/components/form_input/formInput.tsx
+++ src/shared/components/form_input/formInput.tsx
@@ -6,37 +6,48 @@ interface IFormInput extends React.InputHTMLAttributes<HTMLInputElement> {
   labelVisible: boolean;
   changeValue: (value: string) => void;
   enterCallback: () => void;
+  validationErrorMessage?:string;
 }
-const FormInput =({id,value,labelTitle,isRequired,labelVisible,changeValue,enterCallback, ...rest}:IFormInput)=> {
-  const LabelClass = labelVisible ? cl.inputLable:'visually-hidden';
+const FormInput =({labelTitle,isRequired,labelVisible,changeValue,enterCallback,validationErrorMessage,...rest}:IFormInput)=> {
   function handleInputChange(e: React.ChangeEvent<HTMLInputElement>){
     e.preventDefault();
-    changeValue(e.target.value);
+    changeValue((e.target.value));
   };
-
   function handleInputOnEnter(e: React.KeyboardEvent){
     if (e.key === 'Enter' || e.keyCode === 13) {
       enterCallback()
       e.preventDefault();
       return false;
     }
-  }
+  };
+  function extendRequiredPlaceholder(isRequired: boolean, placeholder: string | undefined){
+    if(placeholder) {
+      return isRequired ? `${placeholder} *` : placeholder;
+    } 
+    return '';
+  };
 
   return (
     <div>
       <label
-        className={LabelClass}
-        htmlFor={id}
+        className={cl.inputLabel}
+        htmlFor={rest.id}
       >
-        {labelTitle}{isRequired? <Required/>:''}
       </label>
-      <input className={cl.input} id={id} value={value} onChange={handleInputChange} onKeyDown={handleInputOnEnter} {...rest} required/>
+      <span className={cl.errOutput}>
+        {validationErrorMessage || ''}
+        &nbsp;
+      </span>
+      <input
+        {...rest}
+        className={cl.input}
+        onChange={handleInputChange}
+        onKeyDown={handleInputOnEnter}
+        placeholder={extendRequiredPlaceholder(isRequired, rest.placeholder)}
+        title={rest.title}
+        />
     </div>
   )
 };
-
-const Required =()=>{
-  return <span className={cl.required} aria-label='required field'>*</span>
-}
 
 export default FormInput;
